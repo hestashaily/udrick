dde929125aef43addae4a2549a157afa037d98da
Rajat93gupta <rajatkumr3991@gmail.com>
2025-08-01 17:03:02 +0530

    changes again

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@
 
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import { Suspense } from "react";
 
 export default function MainLayout({
   children,
@@ -11,7 +12,9 @@ export default function MainLayout({
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
+      <Suspense fallback={<div>Loading...</div>}>
       <main className="min-h-screen mt-[76px]">{children}</main>
+      </Suspense>
 
       <Footer />
     </div>
--- src/app/(seller)/layout.tsx
+++ src/app/(seller)/layout.tsx
@@ -2,6 +2,7 @@
 
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import { Suspense } from "react";
 
 export default function SellerLayout({
   children,
@@ -11,7 +12,9 @@ export default function SellerLayout({
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
+      <Suspense fallback={<div>Loading...</div>}>
       <main className="">{children}</main>
+      </Suspense>
 
       <Footer />
     </div>
